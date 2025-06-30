8dbe40216ddf3603ac6118b2f119162f7d4551c5
lord-cyclone100 <sang66deb@gmail.com>
2025-06-30 17:17:12 +0530

    Completed Cal Hero

--- src/components/FifthPage.jsx
+++ src/components/FifthPage.jsx
@@ -3,7 +3,41 @@ import gsap from "gsap"
 import { useRef } from "react"
 
 export const FifthPage = () =>{
-  const videoRef = useRef(null)
+  const videoRef = useRef(null);
+
+
+
+
+
+
+
+    const handleMouseEnter = () => {
+    gsap.to('#vice-city', {
+      scale:1.01,
+      rotation: 1, // Negative for left rotation
+      duration: 0.3,
+      ease: "power2.out"
+    })
+  }
+
+  const handleMouseLeave = () => {
+    gsap.to('#vice-city', {
+      scale:1,
+      rotation: 0, // Back to original position
+      duration: 0.3,
+      ease: "power2.out"
+    })
+  }
+
+
+
+
+
+
+
+
+
+
   useGSAP(()=>{
     const tl = gsap.timeline({
       scrollTrigger:{
@@ -56,10 +90,40 @@ export const FifthPage = () =>{
     ease:'none'
   })
 
+  const tl2 = gsap.timeline({
+      scrollTrigger:{
+        trigger:'#fade1',
+        start:'bottom bottom',
+        end:'bottom center',
+        scrub:true
+      }
+    })
+
+    tl2.to('#fade1',{
+      backgroundImage:"linear-gradient(to bottom right, #28375c, #274a6e)",
+      duration:2,
+      ease:'none'
+    })
+
+    const tl3 = gsap.timeline({
+      scrollTrigger:{
+        trigger:'#vice-city',
+        start:'top 80%',
+        end:'bottom 10%',
+        scrub:true
+      }
+    })
+
+    tl3.to('#hey',{
+      backgroundImage:"linear-gradient(to right, #694775, #876fb4)",
+      duration:2,
+      ease:'none'
+    })
+
   },[])
   return(
     <>
-      <div className="w-full h-[408vh] relative bg-emerald-900">
+      <div id="hola" className="w-full h-[408vh] relative bg-emerald-900">
         <img src="./images/carlbg.webp" alt="" id="fade" className="absolute z-2" />
         <div id="overlay6" className="absolute w-[inherit] h-[138vh] bg-gradient-to-r from-[#131845]  to-transparent top-0 opacity-50 z-4"></div>
         {/* <div id="overlay6" className="absolute w-[inherit] h-[138vh] bg-gradient-to-b from-transparent  to-[#1a243c] top-0 opacity-50 z-3"></div> */}
@@ -95,6 +159,39 @@ export const FifthPage = () =>{
             </div>
           </div>
         </div>
+      </div>
+      <div id="hey" className="relative h-[350vh] w-full bg-gradient-to-tr from-[#28375c] to-[#274a6e] overflow-x-hidden">
+        
+        <div className="w-110 h-[120vh] bg-lime-400 absolute top-10 left-[20%]">
+          <img src="./images/cal-bar.jpg" className="object-cover w-full h-full object-[58%_center]" alt="" />
+        </div>
+        <div className="flex flex-col gap-40">
+          <div className="size-110 bg-lime-700 absolute right-[18.5%] top-70">
+            
+            <img src="./images/cal-pool.jpg" className="object-cover w-full h-full object-[16%_center]" alt="" />
+          </div>
+          <div className="w-1/2 flex flex-col gap-6 -right-30 top-200 absolute">
+            <h3 className="font-gta2 text-[#91dfec] text-4xl w-[20vw]">
+              The psychopaths are in charge. Get used to it.
+            </h3>
+            <p className="font-gta2 text-white text-2xl w-[28vw]">
+              Cal is at the low tide of America and happy there. Casual paranoia loves company, but his friend Jason has bigger plans.
+            </p>
+          </div>
+        </div>
+        <div className="absolute flex items-center gap-20 top-350 left-[50%] transform -translate-x-1/2">
+          <div className="">
+            <img src="./images/visit-leonida-logo.svg" className="h-30" alt="" />
+          </div>
+          <div className="font-gta2 text-white text-3xl w-[23vw]">
+            <p>
+              Tour a few of the must-see destinations across the sunshine state.
+            </p>
+          </div>
+        </div>
+        {/* <div className="absolute transform left-1/2 top-400 h-dvh bg-amber-300"> */}
+          <img id="vice-city" src="./images/vice-city-postcard.svg" className="h-[98vh] absolute top-400 left-1/2 transform -translate-x-1/2" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} alt="" />
+        {/* </div> */}
       </div>
     </>
   )
