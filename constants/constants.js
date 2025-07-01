6b0c8a524d6286ec8e3d5054319cce5ca6e49a6e
lord-cyclone100 <sang66deb@gmail.com>
2025-07-01 12:49:07 +0530

    Completed footer

---
+++ constants/constants.js
@@ -0,0 +1,8 @@
+export const footerDetails = [
+  "Corporate",
+  "Privacy",
+  "Cookie Settings",
+  "Cookie Policy",
+  "Legal",
+  "Do Not Sell or Share My Personal Information"
+]
--- src/components/TwelvethPage.jsx
+++ src/components/TwelvethPage.jsx
@@ -1,10 +1,12 @@
+import { footerDetails } from "../../constants/constants"
+
 export const TwelvethPage = () => {
   return(
     <>
       <div className="text-white w-[100%] h-[50vh] bg-gradient-to-t from-[#111117] via-[#16141f] to-[#1b1828] flex">
-        <div className="size-full flex items-center flex-col gap-8 mt-0">
+        <div className="size-full flex items-center flex-col gap-8 mt-0 font-gta1">
           <div className="flex items-center gap-8 text-2xl">
-            <p className="font-gta1">Wishlist Now</p>
+            <p className="">Wishlist Now</p>
             <button className="rounded-4xl bg-[#2a2a30]">
               <img src="./images/ps5logo.png" className="h-4 my-4 mx-6" alt="" />
             </button>
@@ -14,15 +16,21 @@ export const TwelvethPage = () => {
           </div>
           <div className="w-[60vw] h-24 rounded-[500px] border-1 border-[#ffffff30] flex items-center justify-center gap-8 mx-20">
             <img src="./images/rockstar-games-logo.svg" alt="" className="h-6" />
-            <h6 className="font-gta1 font-bold text-[110%]">GET ROCKSTAR PROPAGANDA</h6>
-            <p className="font-gta1 w-1/2 text-[82%]">Get the latest game announcements, updates on special events and offers, and much more from Rockstar Games</p>
+            <h6 className=" font-bold text-[110%]">GET ROCKSTAR PROPAGANDA</h6>
+            <p className=" w-1/2 text-[82%]">Get the latest game announcements, updates on special events and offers, and much more from Rockstar Games</p>
           </div>
-          <div>
-            <p>privacy</p>
+          <div className="flex text-[85%] gap-8 text-[#ffffff80]">
+            {
+              footerDetails.map((item,idx)=>(
+                <p key={idx}>{item}</p>
+              ))
+            }
           </div>
-          <div className="flex gap-20 items-center">
+          <div className="flex gap-8 items-center justify-center">
             <img src="./images/rp.svg" alt="" />
-            <p>para</p>
+            <p className="w-[30vw]">
+              May contain content inappropriate for children. Visit <u>esrb.org</u> for rating information.
+            </p>
           </div>
         </div>
       </div>
